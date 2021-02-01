c55ccaa06d012a98fca2cdbcfaa114be87082f1e
Luwengier <g217881111@gmail.com>
2021-02-01 23:38:15 +0800

    Communicate with Props

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 // Create a react component
 const App = () => {
-  const buttonText = {text: 'Click Me!'};
-  const labelText = 'Enter name:';
-
   return (
-    <div>
-      <label htmlFor="name" className="label">
-        {labelText}
-      </label>
-      <input id="name" type="text" />
-      <button style={{backgroundColor: 'blue', color: 'white'}}>
-        {buttonText.text}
-      </button>
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warnning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 2:00AM"
+        content="I like the subject"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="Victer"
+        timeAgo="Today at 5:50PM"
+        content="I like the writing"
+        avatar={faker.image.image()}
+      />
     </div>
   );
 };
