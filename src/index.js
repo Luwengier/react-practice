46bbecbd2b05a01062a48b46a3f193174185f575
Luwengier <g217881111@gmail.com>
2021-02-12 23:28:31 +0800

    Specify default props

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ class App extends React.Component {
       position => this.setState({lat: position.coords.latitude }),
       err => this.setState({errorMessage: err.message})
     );
-  }
+  };
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
@@ -23,7 +23,15 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  };
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   }
-}
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
